0af73164572b765ce041ab0875a12c84ef8878e4
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-06 10:18:41 +0100

    create conection to database and funciton to start api rest

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import "./loadEnvironment";
+import connectDatabase from "./database";
+import startServer from "./server/startServer";
 
+const port = process.env.PORT ?? 4500;
+const urlMongo = process.env.MONGOURL as string;
+
+(async () => {
+  try {
+    await startServer(+port);
+    await connectDatabase(urlMongo);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
