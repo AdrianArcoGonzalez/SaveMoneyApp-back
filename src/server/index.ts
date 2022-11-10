06464f8563b102e67d464ad968d4d1ac653274ed
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-10 11:40:59 +0100

    installed cords and add cors middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,15 @@ import "../loadEnvironment";
 import express from "express";
 import chalk from "chalk";
 import Debug from "debug";
+import cors from "cors";
 
 const debug = Debug("SaveMoneyApp:IndexServer");
 
 const app = express();
-app.disable("x-powered-by");
-app.use(express.json());
 
+app.disable("x-powered-by");
+app.use(cors());
+app.use(express.json());
 app.use((req, _res, next) => {
   debug(chalk.blue(`A request arrived to ${req.url}`));
   next();
