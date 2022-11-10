38a7a175bb187cc2d87731b92f3d3db1073b6847
Adrian Arco Gonzalez <adrian.arco.gonzalez@gmail.com>
2022-11-10 21:11:43 +0100

    add hashcomparer function and create token

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -1,11 +1,13 @@
 interface Environments {
   databaseUrl: string;
   port: number;
+  secret: string;
 }
 
 const environments: Environments = {
   databaseUrl: process.env.MONGOURL,
   port: +process.env.PORT,
+  secret: process.env.SECRET,
 };
 
 export default environments;
