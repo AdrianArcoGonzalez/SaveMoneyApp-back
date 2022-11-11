31c9b0248dabcaa1019b5353527fc1a40cf1be2d
Ivan Garcia <ivanggv2@gmail.com>
2022-11-11 17:33:59 +0100

    setted format to the data from DB

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,6 +6,19 @@ const debug = Debug("SaveMoneyApp:conectDatabase:");
 
 const connectDatabase = (mongoUrl: string) =>
   new Promise((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        const newDocument = { ...ret };
+
+        // eslint-disable-next-line no-underscore-dangle
+        delete newDocument.__v;
+        // eslint-disable-next-line no-underscore-dangle
+        delete newDocument._id;
+        delete newDocument.password;
+        return newDocument;
+      },
+    });
     mongoose.connect(mongoUrl, (error) => {
       if (error) {
         debug(chalk.red("Error conecting to database"));
